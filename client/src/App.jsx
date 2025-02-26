2a46af17c1497737813483d017e6768bd244da3c
PRIYANSHU RAMI <ramipriyanshu2004@gmail.com>
2025-02-26 20:17:15 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='font-bold text-2xl text-yellow-300'>
+      React App
+    </main>
+  ); 
+}
+
+export default App
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const registerUser = require('../controller/registerUser')
 const checkEmail = require('../controller/checkEmail')
 const checkPassword = require('../controller/checkPassword')
+const userDetails = require('../controller/userDetails')
+const logout = require('../controller/logout')
+const updateUserDetails = require('../controller/updateUserDetails')
 
 const router = express.Router()
 
@@ -11,5 +14,11 @@ router.post('/register', registerUser)
 router.post('/email', checkEmail)
 //for checking user password
 router.post('/password', checkPassword)
+//for login user details
+router.get('/user-details', userDetails)
+//for logout
+router.get('/logout', logout)  
+//for updating user details
+router.post('/update-user', updateUserDetails) 
 
 module.exports = router
